a27e1beaeab9fa9ad30d8540e28f4d03d26fbb9a
wbvg-mac <will@mylittleshowpony.com>
2013-07-21 17:45:23 +1000

    drag and drop bookings index working

--- app/assets/javascripts/roomselector.js
+++ app/assets/javascripts/roomselector.js
@@ -1,11 +1,82 @@
-
 $(document).ready(function () {
+
 
 $( ".draggable" ).draggable({
       revert: 'invalid'
     });
+
     $( "#droppable" ).droppable({
       accept: function( event, ui) {
+
+        // Reject drops if room is booked.
+        return ! $(this).is('.booked');
+      },
+      activeClass: "ui-state-hover",
+      hoverClass: "ui-state-active",
+      revert: true,
+      drop: function( event, ui ) {
+            $( this ).addClass( "booked" );
+      }
+    });
+
+        $( "#droppable" ).droppable({
+      accept: function( event, ui) {
+        // Reject drops if room is booked.
+        return ! $(this).is('.booked');
+      },
+      activeClass: "ui-state-hover",
+      hoverClass: "ui-state-active",
+      revert: true,
+      drop: function( event, ui ) {
+            $( this ).addClass( "booked" );
+      }
+    });
+
+       $( "#droppable2" ).droppable({
+      accept: function( event, ui) {
+
+        // Reject drops if room is booked.
+        return ! $(this).is('.booked');
+      },
+      activeClass: "ui-state-hover",
+      hoverClass: "ui-state-active",
+      revert: true,
+      drop: function( event, ui ) {
+            $( this ).addClass( "booked" );
+      }
+    });
+
+       $( "#droppable3" ).droppable({
+      accept: function( event, ui) {
+
+        // Reject drops if room is booked.
+        return ! $(this).is('.booked');
+      },
+      activeClass: "ui-state-hover",
+      hoverClass: "ui-state-active",
+      revert: true,
+      drop: function( event, ui ) {
+            $( this ).addClass( "booked" );
+      }
+    });
+
+       $( "#droppable-pres" ).droppable({
+      accept: function( event, ui) {
+
+        // Reject drops if room is booked.
+        return ! $(this).is('.booked');
+      },
+      activeClass: "ui-state-hover",
+      hoverClass: "ui-state-active",
+      revert: true,
+      drop: function( event, ui ) {
+            $( this ).addClass( "booked" );
+      }
+    });
+
+          $( "#droppable-kitchen" ).droppable({
+      accept: function( event, ui) {
+
         // Reject drops if room is booked.
         return ! $(this).is('.booked');
       },
