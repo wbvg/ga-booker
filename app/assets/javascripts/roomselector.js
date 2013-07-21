0b8c0a6159738a2827fbe4298d4d5d2bb49c952d
wbvg-mac <will@mylittleshowpony.com>
2013-07-22 08:43:17 +1000

    style a and fixed link on footer

--- app/assets/javascripts/roomselector.js
+++ app/assets/javascripts/roomselector.js
@@ -17,20 +17,9 @@ $( ".draggable" ).draggable({
       drop: function( event, ui ) {
             $( this ).addClass( "booked" );
       }
+
     });
 
-        $( "#droppable" ).droppable({
-      accept: function( event, ui) {
-        // Reject drops if room is booked.
-        return ! $(this).is('.booked');
-      },
-      activeClass: "ui-state-hover",
-      hoverClass: "ui-state-active",
-      revert: true,
-      drop: function( event, ui ) {
-            $( this ).addClass( "booked" );
-      }
-    });
 
        $( "#droppable2" ).droppable({
       accept: function( event, ui) {
